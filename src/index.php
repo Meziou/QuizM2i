e2acd32b514b951b2ff02859c37887fad68d9de0
chems meziou <chemsbook@chemss-MacBook-Air.local>
2024-06-18 11:49:49 +0200

    end editing question and quiz

--- src/index.php
+++ src/index.php
@@ -116,12 +116,7 @@ $quizs = $result->fetch_all(MYSQLI_ASSOC);
     <?php endforeach; ?>
 </div>
 
-<script>
-    function setUpdateFormData(id, name) {
-        document.getElementById('updateQuizId').value = id;
-        document.getElementById('updateQuizName').value = name;
-    }
-</script>
+
 
 <?php
 $title = 'Quizs';
